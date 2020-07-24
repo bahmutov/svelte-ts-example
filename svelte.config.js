35dc0c9cb64a2a65e4952c644b82397637452d14
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-07-24 16:14:32 -0400

    initial

---
+++ svelte.config.js
@@ -0,0 +1,7 @@
+// svelte.config.js - NOTE: you cannot use the new "import x from y" and "export const" syntax in here.
+const sveltePreprocess = require('svelte-preprocess');
+
+module.exports = {
+    preprocess: sveltePreprocess(),
+    // ...other svelte options
+};
