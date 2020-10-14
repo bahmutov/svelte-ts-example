2f6aaca1da92f347d411cb9cd513e258870f8ea9
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-10-13 21:23:27 -0400

    add link to the issue

--- src/App.spec.ts
+++ src/App.spec.ts
@@ -2,6 +2,8 @@ import App from './App.svelte'
 import { mount } from 'cypress-svelte-unit-test'
 it('shows greeting', () => {
   mount(App, {
+    // don't have type to pass props yet
+    // https://github.com/bahmutov/cypress-svelte-unit-test/issues/206
     // @ts-ignore
     props: {
       name: 'World',
